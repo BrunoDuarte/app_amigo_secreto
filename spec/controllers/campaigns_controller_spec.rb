8f7239fc586499618cdd6cbcf05e1ab4fffef84b
BrunoDuarte <frank.bruno555@gmail.com>
2019-11-03 12:22:11 -0300

    Test controller Campaign

--- spec/controllers/campaigns_controller_spec.rb
+++ spec/controllers/campaigns_controller_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe CampaignsController, type: :controller do
   include Devise::Test::ControllerHelpers
 
   before(:each) do
+    # request.env["HTTP_ACCEPT"] = 'application/json'
+
     @request.env["devise.mapping"] = Devise.mappings[:user]
     @current_user = FactoryBot.create(:user)
     sign_in @current_user
